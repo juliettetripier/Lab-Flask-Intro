5b9f29f41e14b3f63732b59684d5865dbf7ced95
Juliette <juli.tripier@gmail.com>
2023-09-12 15:49:24 -0400

    Finished lab

--- server.py
+++ server.py
@@ -13,12 +13,16 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+INSULTS = [
+    'idiot', 'terrible', 'garbage', 'trash', 'horrible', 'awful',
+    'dumb', 'the worst']
+
 
 @app.route('/')
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return '<!doctype html><html>Hi! This is the home page. <a href="/hello"> Click here to go to the Hello page.</a>  </html>'
 
 
 @app.route('/hello')
@@ -37,6 +41,10 @@ def say_hello():
           What's your name? <input type="text" name="person">
           <input type="submit" value="Submit">
         </form>
+        <form action="/diss">
+          What's your name? (evil) <input type="text" name="person">
+          <input type="submit" value="Submit">
+        </form>
       </body>
     </html>
     """
@@ -49,7 +57,7 @@ def greet_person():
     player = request.args.get("person")
 
     compliment = choice(AWESOMENESS)
-
+    
     return f"""
     <!doctype html>
     <html>
@@ -62,8 +70,25 @@ def greet_person():
     </html>
     """
 
+@app.route('/diss')
+def diss_person():
+    
+    player = request.args.get("person")
+    insults = choice(INSULTS)
 
+
+    return f"""
+      <!doctype html>
+      <html>
+        <head>
+          <title>An Insult</title>
+        </head>
+        <body>
+          Hi, {player}! I think you're {insults}!
+        </body>
+      </html>
+    """
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5001)
